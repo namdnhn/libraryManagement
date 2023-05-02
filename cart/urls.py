a4be653f3c9962fa34cf32a336c61a460f80dddc
namdnhn <donhuhoangnam.work@gmail.com>
2023-05-02 17:55:03 +0700

    edit FE and add transaction

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('add/<int:id>/', views.cart_add, name='cart_add'),
     path('item_clear/<int:id>/', views.item_clear, name='item_clear'),
     path('cart-detail/',views.cart_detail,name='cart_detail'),
+    path('transaction-view/',views.view_transaction,name='transaction_view'),
+    path('transaction-create/',views.create_transaction,name='transaction_create'),
+    path('transaction-list/',views.list_transaction,name='transaction_list'),
 ]
